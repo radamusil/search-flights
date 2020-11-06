37da13660432c2af52a2c7351d9df2fbc5d24b53
radamusil <64097743+radamusil@users.noreply.github.com>
2020-11-06 11:15:04 +0100

    fetching

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+  const [flights, setFlights] = useState([]);
+  const [to, setTo] = useState('');
+  const [from, setFrom] = useState('');
+
+  const handleToChange = (e) => {
+    setTo(e.currentTarget.name);    
+    }  
+
+  const handleFromChange = (e) => {
+            setFrom(e.currentTarget.name);
+            }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const fetchData = async () => {
+    const url = `https://api.skypicker.com/flights?flyFrom=${from}&to=${to}&dateFrom=18/11/2020&dateTo=12/12/2020&partner=picky&v=3`;
+    const response = await fetch(url);
+
+    const resp = await response.json();
+    setFlights(resp);
+    console.log(flights);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
