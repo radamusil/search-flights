acf123aef41c17519a510c55fc0e13f14d44a1ad
Šimon Cipro <simon.cipro@gmail.com>
2020-11-06 11:12:31 +0100

    search bar w/ two airport for testing

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+
+const SearchBar = (props) => {
+    const [dropdownToOpen, setDropdownToOpen] = useState(false);
+    const [dropdownFromOpen, setDropdownFromOpen] = useState(false);
+    const [to, setTo] = useState('');
+    const [from, setFrom] = useState('');
+
+    const toggleTo = () => setDropdownToOpen(prevState => !prevState);
+    const toggleFrom = () => setDropdownFromOpen(!dropdownFromOpen);
+
+    const handleToChange = (e) => {
+        setTo(e.currentTarget.name);
+    }
+
+    const handleFromChange = (e) => {
+        setFrom(e.currentTarget.name);
+    }
+
+    return (
+        <div className="search-bar">
+              
+            {/* select to airport - passed as a API name of the airport   */}
+            <Dropdown isOpen={dropdownToOpen} toggle={toggleTo}>
+                <DropdownToggle caret>
+                    To
+                </DropdownToggle>
+                <DropdownMenu>
+                    <DropdownItem onClick={handleToChange} name='PRG'>PRG</DropdownItem>
+                    <DropdownItem onClick={handleToChange} name='LGW'>LGW</DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
+
+            {/* select from passed as a API name of the airport*/}
+            <Dropdown isOpen={dropdownFromOpen} toggle={toggleFrom}>
+                <DropdownToggle caret>
+                    From
+                </DropdownToggle>
+                <DropdownMenu>
+                    <DropdownItem onClick={handleFromChange} name='PRG'>PRG</DropdownItem>
+                    <DropdownItem onClick={handleFromChange} name='LGW'>LGW</DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
+        </div>
+    )
+
+}
+
+export default SearchBar;
